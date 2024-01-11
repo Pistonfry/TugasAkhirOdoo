5533a879998b5b8ffea50ad8807f9f1d0867bf01
Pistonfry <christianfiestyawan@gmail.com>
2024-01-11 17:05:57 +0700

    Video 3 Compute Fields Done

--- odoo/addons/om_hospital/models/patient.py
+++ odoo/addons/om_hospital/models/patient.py
@@ -1,20 +1,44 @@
-from odoo import api, fields, models
+from odoo import api, fields, models,_
+from odoo.exceptions import ValidationError
 
 class HostpitalPatient(models.Model):
     _name = "hospital.patient"
     _inherit='mail.thread'
     _description = "Patient Records"
 
-    name=fields.Char(string='Name',required=True, tracking=True)
-    age = fields.Integer(string="Age", tracking=True)
-    is_child = fields.Boolean(string="Is Child ?", tracking=True)
+    name=fields.Char(string='Name',
+                     required=True, 
+                     tracking=True)
+    age = fields.Integer(string="Age", 
+                         tracking=True)
+    is_child = fields.Boolean(string="Is Child ?", 
+                              tracking=True)
     notes = fields.Text(string="Notes")
     gender = fields.Selection([
         ('male', "Male"),
         ('female', "Female"),
         ('other', "Others")],
-        string="Gender", tracking=True)
+        string="Gender", 
+        tracking=True)
+    capitalized_name=fields.Char(string='Capitalized Name', 
+                                 compute='_compute_capitalized_name', 
+                                 store=True)
     
+    #Check if it's child, age must be filled
+    @api.constrains('is_child','age')
+    def _check_child_age(self):
+        for rec in self:
+            if rec.is_child and rec.age == 0:
+                raise ValidationError(_("Age has to be recoded !"))
+
+    @api.depends('name')
+    def _compute_capitalized_name(self):
+        for rec in self:
+            if rec.name:
+                rec.capitalized_name=self.name.upper()
+            else:
+                rec.capitalized_name=''
+
     @api.onchange('age')
     def _onchance_age(self):
         if self.age <=10:
